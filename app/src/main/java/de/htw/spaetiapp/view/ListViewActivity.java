63e2a429e8ef9271d73e167e7a235e9bd281cbbe
Ioakeim <44837904+Ioakeim@users.noreply.github.com>
2019-11-13 17:02:47 +0100

    added buttons

---
+++ app/src/main/java/de/htw/spaetiapp/view/ListViewActivity.java
@@ -0,0 +1,16 @@
+package de.htw.spaetiapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import de.htw.spaetiapp.R;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
