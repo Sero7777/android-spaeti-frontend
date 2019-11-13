e20215b5288b3795a4c1d2e9d19a3af7e72ef19e
AndKaz97 <you@example.com>
2019-11-13 17:24:47 +0100

    Spaeti Class getter/setter added

--- app/src/main/java/de/htw/spaetiapp/models/Spaeti.java
+++ app/src/main/java/de/htw/spaetiapp/models/Spaeti.java
@@ -1,4 +1,95 @@
 package de.htw.spaetiapp.models;
 
 public class Spaeti {
+
+    private String name;
+    private String openingTime;
+    private String closingTime;
+    private String description;
+    private float lon;
+    private float lat;
+    private String streetName;
+    private String city;
+    private int zip;
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOpeningTime() {
+        return openingTime;
+    }
+
+    public String getClosingTime() {
+        return closingTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOpeningTime(String openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public void setClosingTime(String closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLon(float lon) {
+        this.lon = lon;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
