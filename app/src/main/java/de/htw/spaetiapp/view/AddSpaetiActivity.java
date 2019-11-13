beb07b625bf3e9cbfc0df031aa5b84287b1adcb0
Ioakeim <44837904+Ioakeim@users.noreply.github.com>
2019-11-13 16:28:42 +0100

    added floating button on map activity

--- app/src/main/java/de/htw/spaetiapp/view/AddSpaetiActivity.java
+++ app/src/main/java/de/htw/spaetiapp/view/AddSpaetiActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import de.htw.spaetiapp.R;
+
 public class AddSpaetiActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/de/htw/spaetiapp/view/MapsActivity.java
+++ app/src/main/java/de/htw/spaetiapp/view/MapsActivity.java
@@ -1,4 +1,4 @@
-package de.htw.spaetiapp;
+package de.htw.spaetiapp.view;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import de.htw.spaetiapp.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
