f62f2adac30dbaffd7fe4a5a46ea46c1d55b48e4
Ioakeim <44837904+Ioakeim@users.noreply.github.com>
2019-11-13 16:16:29 +0100

    updated AddSpaetiActivity

---
+++ app/src/main/java/de/htw/spaetiapp/view/AddSpaetiActivity.java
@@ -0,0 +1,14 @@
+package de.htw.spaetiapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddSpaetiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_spaeti);
+    }
+}
