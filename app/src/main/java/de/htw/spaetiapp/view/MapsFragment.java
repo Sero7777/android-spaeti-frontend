6fb113982715d61a5c6ab8471889ae408321c626
Dorothee Brodowsky <s0564778@htw-berlin.de>
2019-11-27 15:38:03 +0100

    added location permission and my location button

--- app/src/main/java/de/htw/spaetiapp/view/MapsFragment.java
+++ app/src/main/java/de/htw/spaetiapp/view/MapsFragment.java
@@ -1,13 +1,20 @@
 package de.htw.spaetiapp.view;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,9 +30,14 @@ import de.htw.spaetiapp.R;
  */
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    MapView mapView;
-    View view;
+    private GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
+
+    private static final int MY_REQUEST_INT = 177;
+
+
+    private static final String TAG = "MapFragment";
 
 
     public MapsFragment() {
@@ -33,24 +45,37 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     }
 
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_maps, container, false);
+        mMapView = mView.findViewById(R.id.map);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+
+        // Get the button view
+        View locationButton = ((View) mView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+
+        // and next place it, for exemple, on bottom right (as Google Maps app)
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+       // position on right bottom
+       rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        rlp.setMargins(0, 0, 30, 30);
+
+        return mView;
 
 
-        // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_maps, container, false);
-        return view;
     }
 
-    public void onViewCreated(View view, Bundle savedInstanceState){
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mapView = view.findViewById(R.id.map);
-        if (mapView!=null){
-            mapView.onCreate(null);
-            mapView.onResume();
-            mapView.getMapAsync(this);
-        }
+
+
     }
 
 
@@ -63,12 +88,31 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mGoogleMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+
+
+            /* check location's permission.*/
+
+            if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //code for permission not granted.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION}, MY_REQUEST_INT);
+                }
+                return;
+            } else {
+                //code for permission granted.
+                mGoogleMap.setMyLocationEnabled(true);
+                //mGoogleMap.clear();
+                //mGoogleMap.setPadding(0,1600,0,0);
+
+
+            }
+
+
+        }
 }
